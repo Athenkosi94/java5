1f01a1dc05984127d4db1ba1a1076b2457d3ea60
user <athimbityana@gmail com>
2022-06-06 15:44:03 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int duration = 355;
+        int hours;
+        int minutes;
+
+        hours =duration/60;
+        minutes = duration%60;
+
+        System.out.println(" it takes " + hours + " hours " + minutes + " " + "minutes");
+    }
+}
